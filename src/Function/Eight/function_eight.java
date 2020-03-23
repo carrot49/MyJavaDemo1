2c54c8973aecc10c6e2f593debbc1daa632c99fe
carrot29 <1506923339@qq.com>
2020-03-24 02:00:07 +0800

    Initial Commit
    Signed-off-by: carrot29 <1506923339@qq.com>

--- src/Function/Eight/function_eight.java
+++ src/Function/Eight/function_eight.java
@@ -10,6 +10,7 @@ public class function_eight {
 
     public static void bassic_function(){
 
+        List l1 = new ArrayList();
         Collection c1 = new ArrayList();
         person p1 = new person("yyz",20);
         person p2 = new person("yyz",21);
@@ -17,6 +18,10 @@ public class function_eight {
         c1.add(p1);
         c1.add(p2);
         c1.add(p3);
+
+        l1.add(p3);
+        l1.add(p2);
+        l1.add(p1);
 
         System.out.println(p2);
         System.out.println(c1.size());
@@ -26,15 +31,20 @@ public class function_eight {
             System.out.println(obj1[i]);
         }
 
-        System.out.println("迭代器：");
-        Iterator it1 = c1.iterator();
 
-        /*
+
+        System.out.println("迭代器：");
+
+        Iterator it1 = l1.iterator();
         while ((it1.hasNext())){
             Object returnElement = it1.next();
             System.out.println(returnElement);
         }
-        */
+
+        for(int i = 0;i<l1.size();i++){
+            Object element = l1.get(i);
+            System.out.println(element);
+        }
 
         for(Iterator it2 = c1.iterator();it2.hasNext();){
             Object o2 = it2.next();
